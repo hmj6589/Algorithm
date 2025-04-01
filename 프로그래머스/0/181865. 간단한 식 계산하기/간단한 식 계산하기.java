ad2b4287fda21124caafc1431a74152699fdc19d
황민지 <139426988+hmj6589@users.noreply.github.com>
2025-04-02 02:12:43 +0900

    [level 0] Title: 간단한 식 계산하기, Time: 0.54 ms, Memory: 81 MB -BaekjoonHub

--- 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.java
+++ 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.java
@@ -2,17 +2,17 @@ import java.util.*;
 
 class Solution {
     public int solution(String binomial) {
-        String[] str = binomial.split(" ");
-        List<String> list = new ArrayList<>();
-        for(String i:str){
-            if(!i.isEmpty()) list.add(i);
-        }
-        int num1 = Integer.parseInt(list.get(0));
-        int num2 = Integer.parseInt(list.get(2));
+        String[] str = binomial.trim().split("\\s+");
+        // List<String> list = new ArrayList<>();
+        // for(String i:str){
+        //     if(!i.isEmpty()) list.add(i);
+        // }
+        int num1 = Integer.parseInt(str[0]);
+        int num2 = Integer.parseInt(str[2]);
         
-        if((list.get(1)).equals("+")){
+        if(str[1].equals("+")){
             return num1+num2;
-        } else if((list.get(1)).equals("-")){
+        } else if(str[1].equals("-")){
             return num1-num2;
         } else {
             return num1*num2;
