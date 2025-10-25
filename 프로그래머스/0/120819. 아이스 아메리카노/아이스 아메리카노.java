fce3e0011eac9b7ae7d94a35b87c475db69e6ffe
황민지 <hmj6589@gmail.com>
2025-10-25 17:39:19 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.02 ms, Memory: 80.2 MB -BaekjoonHub

--- 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.java
+++ 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.java
@@ -2,14 +2,7 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int money) {
-        List<Integer> list = new ArrayList<>();
-        
-        list.add(money/5500);
-        list.add(money%5500);
-        
-        int[] answer = new int[list.size()];
-        for(int i=0;i<list.size();i++) answer[i] = list.get(i);
-        
-        return answer;
+        return new int[] {money/5500, money%5500};
+
     }
 }
