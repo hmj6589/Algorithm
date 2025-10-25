8f8c88ad59c29c6d5222d1358cdcda13976b028c
황민지 <hmj6589@gmail.com>
2025-10-25 18:09:12 +0900

    [level 0] Title: 문자열 밀기, Time: 1.67 ms, Memory: 84.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(String A, String B) {
+        
+        for (int i=0;i<A.length();i++) {
+            if ((A.substring(A.length()-i,A.length())+A.substring(0,A.length()-i)).equals(B)) return i;
+        }
+        
+        return -1;
+    }
+}
